42072140ca2765f98a47c5265d14b3f4364d74eb
Raushan Kumar <31020029+raushankumar021993@users.noreply.github.com>
2017-12-19 23:02:43 -0500

    Update CNNModel1.py

--- chestxray/CNNModel1.py
+++ chestxray/CNNModel1.py
@@ -1,15 +1,30 @@
-path_to_image= "C:/Users/raush/Desktop/CNN/images/"
+# Created by Raushan : Start
 
 import os
+import sys
+import inspect
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt 
+from datetime import datetime
+from scipy import misc
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Flatten, Dense, Activation, Dropout
+from keras import backend as K
+
+# For Calculating total Time taken, while running the model
+initialTime = datetime.now()
+# Path to images Folder
+path_to_image= os.path.dirname(os.path.abspath(__file__)) + "\images"
 
 files=os.listdir(path_to_image) 
-
-path_to_csv="C:/Users/raush/Desktop/CNN/Data_Entry_2017.csv"
+# Path to CSV File
+path_to_csv= os.path.dirname(os.path.abspath(__file__)) + "\Data_Entry_2017.csv"
 
 fp=open(path_to_csv)
-
 datam=fp.readlines()[1:]
-
 datam_new=[]
 
 for row in datam:
@@ -22,16 +37,9 @@ for row in datam:
 		temp.append(0)
 	datam_new.append(temp)
 
-
-
-import numpy as np
-from scipy import misc
-import matplotlib.pyplot as plt 
-
 XMatrix=[]
 count =0
 
-#z=0
 for file in files:
 	image = misc.imread(path_to_image+file)
 	if (sum(image.shape) == 2048):
@@ -48,17 +56,6 @@ for file in files:
 			YMatrix.append(row[1])
 
 YMatrix=np.asarray(YMatrix)
-
-
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Flatten, Dense, Activation, Dropout
-from keras import backend as K
-
-#epoch = 20
-#batch_size = 16
-# # Model Starts ****
 input_shape = (1024,1024,1)
 model = Sequential()
 model.add(Conv2D(32, (4, 4), input_shape=input_shape))
@@ -77,16 +74,11 @@ model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
-
-# model = Sequential()
-# model.add(Convolution2D(16, 5, 5, activation ='relu', input_shape=input_shape))
-# model.add(Maxpooling2D(2, 2))
-
-# model.add(Convolution2D(32, 5, 5, activation ='relu'))
-# model.add(Maxpooling2D(2,2))
-
-# Compile the above model 
 model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-	
-model.fit(XMatrix, YMatrix, batch_size=16, epochs=20,verbose=1)
-model.predict(XMatrix, batch_size=16, verbose=20)
+model.fit(XMatrix, YMatrix, batch_size=10, epochs=1,verbose=1)
+model.predict(XMatrix, batch_size=10, verbose=20)
+
+finalTime = datetime.now()
+totalTime = finalTime - initialTime
+print("Total Time the model run is : ",totalTime)
+# Created by Raushan : End
